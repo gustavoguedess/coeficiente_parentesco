ce13f0340951aab1a8f3dfb07c8a96db5597efd1
Gustavo Guedes <gustavoipguedes@gmail.com>
2023-10-26 00:53:25 +0100

    feat: grau de parentesco funcionando

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #%% Abrir arquivo de entrada e ajuste de dados
 import pandas as pd
 
-df = pd.read_excel('Dados coelhos- ascendentes.xlsx', dtype=str)
+EXCEL_FILE_NAME = 'coelhos.xlsx'
+
+df = pd.read_excel(EXCEL_FILE_NAME, dtype=str)
 df = df.rename(columns={
     'Indivíduo': 'individuo',
     'Pai': 'pai',
@@ -19,6 +21,7 @@ df.sort_values(by=['individuo'], inplace=True)
 df['individuo'] = df['individuo'].str.strip()
 
 df['individuo'].tolist()
+
 # %% Classe Coelho
 from collections import defaultdict
 
@@ -57,44 +60,7 @@ class Coelho:
     def get_sexo(self):
         return self.sexo
     
-
-class CoeficienteParentesco:
-    def __init__(self, coelhos):
-        self.coelhos = coelhos
-        self.coeficientes_parentesco = []
-    
-    def calc_coeficiente_coelho(self, coelho, grau=0):
-        if self.visitados[coelho.nome]:
-            return
-        self.visitados[coelho.nome] = True
-
-        if coelho.get_sexo() == 'F':
-            self.coeficiente_coelho[coelho.nome]+=(1/2)**grau
-        
-        if coelho.get_pai():
-            self.calc_coeficiente_coelho(coelho.get_pai(), grau+1)
-        if coelho.get_mae():
-            self.calc_coeficiente_coelho(coelho.get_mae(), grau+1)
-        for filho in coelho.filhos:
-            self.calc_coeficiente_coelho(filho, grau+1)
-
-    def calcular(self):
-        visitados = set()
-        for coelho in self.coelhos.values():
-            if coelho.get_sexo() == 'M':
-                self.coeficiente_coelho = defaultdict(int)
-                self.visitados = defaultdict(bool)
-                self.calc_coeficiente_coelho(coelho)
-                
-                row = {
-                    'coelhos': coelho.nome,
-                    **self.coeficiente_coelho
-                }
-                self.coeficientes_parentesco.append(row)
-        
-    def get_coeficientes_parentesco(self):
-        return self.coeficientes_parentesco
-#%% Criação da árvore genealógica
+# %% Criação da árvore genealógica
 coelhos = {}
 for index, row in df.iterrows():
     nome = row['individuo']
@@ -114,19 +80,88 @@ for coelho in coelhos.values():
     if coelho.get_pai() == None and coelho.get_mae() == None:
         print(coelho.get_arvore())
 
+#%% Classe CoeficienteParentesco
+class CoeficienteParentesco:
+    def __init__(self, coelhos):
+        self.coelhos = coelhos
+        self.coeficientes_parentesco = []
+    
+    def calc_coeficiente_macho(self, coelho_inicial, coelho_atual=None, grau=0):
+        if coelho_atual == None: coelho_atual = coelho_inicial
+        if self.visitados[coelho_atual.nome]:
+            return
+        self.visitados[coelho_atual.nome] = True
+
+        # Se o coelho atual for do sexo oposto ao coelho inicial
+        if coelho_atual.get_sexo() != coelho_inicial.get_sexo():
+            self.coeficiente_parentesco[coelho_inicial.nome][coelho_atual.nome] += 0.5**grau
+        if coelho_atual.get_pai():
+            self.calc_coeficiente_macho(coelho_inicial, coelho_atual.get_pai(), grau+1)
+        if coelho_atual.get_mae():
+            self.calc_coeficiente_macho(coelho_inicial, coelho_atual.get_mae(), grau+1)
+        for filho in coelho_atual.filhos:
+            self.calc_coeficiente_macho(coelho_inicial, filho, grau+1)
+
+    def calcular(self):
+        femeas = [coelho.nome for coelho in self.coelhos.values() if coelho.get_sexo() == 'F']
+        machos = [coelho.nome for coelho in self.coelhos.values() if coelho.get_sexo() == 'M']
+        print(f'Fêmeas: {femeas}')
+        print(f'Machos: {machos}')
+
+        # Inicializa o mapa de coeficientes de parentesco
+        self.coeficiente_parentesco = defaultdict(dict)
+        for macho in machos:
+            self.coeficiente_parentesco[macho] = {femea: 0 for femea in femeas}
+        # Para cada coelho macho, calcula o coeficiente de parentesco
+        for coelho in self.coelhos.values():
+            if coelho.get_sexo() == 'M':
+                self.visitados = defaultdict(bool)
+                print(f'Calculando coeficientes de parentesco para o coelho {coelho.nome}')
+                self.calc_coeficiente_macho(coelho)
+        
+    def get_coeficientes_parentesco(self):
+        return self.coeficiente_parentesco
+
 # %% Cálculo dos coeficientes de parentesco
 import pandas as pd
 
 cp = CoeficienteParentesco(coelhos)
 cp.calcular()
+
 coeficientes_parentesco = cp.get_coeficientes_parentesco()
 df = pd.DataFrame(coeficientes_parentesco)
-df = df.reindex(sorted(df.columns), axis=1)
-df.insert(0, 'coelhos', df.pop('coelhos'))  
+df.index.name = 'coelhos'
+df = df.applymap(lambda x: round(x, 4))
+df[df<0.0015] = 0
 df
 
+
 #%%
-d = defaultdict(int)
-d['a'] += 1
-d
-# %%
+from openpyxl.formatting.rule import ColorScaleRule
+EXCEL_SHEET_NAME = 'Coeficientes de parentesco'
+
+with pd.ExcelWriter(EXCEL_FILE_NAME, engine="openpyxl", mode='a') as writer:
+    if EXCEL_SHEET_NAME in writer.book.sheetnames:
+        idx = writer.book.sheetnames.index(EXCEL_SHEET_NAME)
+        writer.book.remove(writer.book.worksheets[idx])
+
+    df.to_excel(writer, sheet_name=EXCEL_SHEET_NAME)
+
+    workbook = writer.book
+    worksheet = writer.sheets[EXCEL_SHEET_NAME]
+
+    ultima_celula = worksheet.cell(
+        row=df.shape[0]+1, 
+        column=df.shape[1]+1
+    ).coordinate
+    cell_range = f'B2:{ultima_celula}'
+    
+    worksheet.conditional_formatting.add(cell_range, 
+        ColorScaleRule(
+            start_type='num', start_value=0, start_color='375623',
+            mid_type='num', mid_value=0.25, mid_color='ffc000',
+            end_type='num', end_value=0.7, end_color='FF0000'
+        )
+    )
+
+#%%
